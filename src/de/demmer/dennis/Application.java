57b0fce33ac237e3d042d6b8502ec9a88a3b34b5
Unknown <Unknown@DESKTOP-4KSK0J5.fritz.box>
2022-11-22 11:52:22 +0100

    initial commit

--- src/de/demmer/dennis/Application.java
+++ src/de/demmer/dennis/Application.java
@@ -5,20 +5,100 @@ import java.util.Arrays;
 public class Application {
 
 	public static void main(String[] args) {
+
+//		int[] irgendeinArray = new int[150];
+//		System.out.println(Arrays.toString(irgendeinArray));
+//
+//		
+//		for (int i = 0; i < irgendeinArray.length; i++) {
+//			irgendeinArray[i] = 42;
+//		}
+//
+//		String schönerString = Arrays.toString(irgendeinArray);
+//		System.out.println(schönerString);
+//		
+
+		
+		boolean[] meinRückgabeWert = func1(5);
+		System.out.println(Arrays.toString(meinRückgabeWert));
 		
 		
-		int[] irgendeinArray = {5,6,7,5,7,8,8,0};
+		int[] meinIntArray = func2(meinRückgabeWert);
+		System.out.println(Arrays.toString(meinIntArray));
 		
-		
-		//Gibt nur einen hashwert aus
-		System.out.println("Nicht so gut lesbar: " + irgendeinArray);
-		
-		//gibt den tatsächlichen Inhalt des Arrays aus
-		System.out.println("Gut lesbares Array: " + Arrays.toString(irgendeinArray));
-	
+
 
 	}
 	
 	
 	
+	
+	static boolean[] func1(int arg) {
+		
+		boolean[] result = new boolean[arg];
+		
+		for (int i = 1 ; i <= arg ; i++) {
+			
+			System.out.print(i + "      ");
+			if(i % 2 == 1) {
+				result[i-1] = true;
+			}
+			
+		}
+		
+		System.out.println();
+		
+		
+		return result;
+		
+	}
+	
+	
+	
+	
+	static int[] func2(boolean[] booleanArray) {
+		
+		//Ermittlung der länge des int[]
+		int anzahlTrue = 0;
+		
+		for (int i = 0; i < booleanArray.length; i++) {
+		
+			if(booleanArray[i] == true) {
+				anzahlTrue = anzahlTrue +1 ;
+			}
+	
+		}
+	    // ----------------------
+		
+		
+		int[] result = new int[anzahlTrue];
+		
+
+		int intArrayCounter = 0;
+		
+		for (int i = 0; i < booleanArray.length; i++) {
+			
+			if(booleanArray[i] == true) {
+				result[intArrayCounter] = i + 1;
+				intArrayCounter ++;
+			}
+
+		}
+		
+		
+		return result;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
 }
